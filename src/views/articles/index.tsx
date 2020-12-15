b4e3fc86f2b0a017ac4bf02bc78d2d0147bc413d
unknown <szhao@sciplus.site>
2020-12-15 17:18:23 +0800

    引入node & mongdb

--- src/views/articles/index.tsx
+++ src/views/articles/index.tsx
@@ -1,46 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ArticleUl, ArticleLi, Color } from './style';
 import { Link } from 'react-router-dom'
+import 'whatwg-fetch';
+
+function ListItem(props) {
+  const item = props.item
+  let color = Color()
+  return (
+    <Link to={`/detail/${item.id}`}>
+      <ArticleLi style={{boxShadow: `0 0 8px 2px ${color}`}}>
+        <div className="title">
+          <span style={{color: color}}>{item.title}</span>
+        </div>
+        <div className="desc"><span>{item.desc}</span></div>
+        <div className="date"><span>{item.publishDate}</span></div>
+      </ArticleLi>
+    </Link>
+  )
+}
+
+function ArticleList(props) {
+  const list = props.articles
+  const listItems = list.map((item) => {
+     return <ListItem key={item.id} item={item} />
+  })
+  return (
+    <ArticleUl>
+      { listItems }
+    </ArticleUl>
+  )
+}
+
 export default function () {
-    function ListItem(props) {
-      const item = props.item
-      let color = Color()
-      return (
-        <Link to={`/detail/${item.id}`}>
-          <ArticleLi style={{boxShadow: `0 0 8px 2px ${color}`}}>
-            <div className="title">
-              <span style={{color: color}}>{item.title}</span>
-            </div>
-            <div className="desc"><span>{item.desc}</span></div>
-            <div className="date"><span>{item.publishDate}</span></div>
-          </ArticleLi>
-        </Link>
-      )
-    }
-    
-    function ArticleList(props) {
-      const list = props.articles
-      const listItems = list.map((item) => {
-         return <ListItem key={item.id} item={item} />
-      })
-      return (
-        <ArticleUl>
-          { listItems }
-        </ArticleUl>
-      )
-    }
-    const articles = []
-    for(let i=0; i< 10; i++) {
-      articles.push({
-        id: `${i+1}`,
-        title: `我的第${i+1}篇文章`,
-        desc: "关于react的一些使用技巧、分享给大家，欢迎大家多多指教！",
-        publishDate: "2020-01-23"
-      })
-    } 
-    return (
-      <div className="content-wrap">
-        <ArticleList articles={articles} />
-      </div>
-    );
-  };
+  const [ articles, setArticles ] = useState([]);
+
+  function getlistFetch() {
+    fetch('/api/list',{
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response)=> response.json())
+    .then((data) => {
+        let resData = data ? data.articles || [] : []
+        setArticles(resData)
+        console.log(resData)
+    }).catch(e=>{console.log(e);})
+  }
+
+  useEffect(() => {
+    // getlistFetch()
+    fetch('/api/list',{
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response)=> response.json())
+    .then((data) => {
+        let resData = data ? data.articles || [] : []
+        setArticles(resData)
+        console.log(resData)
+    }).catch(e=>{console.log(e);})
+  }, ['resData'])
+
+  return (
+    <div className="content-wrap">
+      <ArticleList articles={articles} />
+    </div>
+  );
+};
